60ee79579affae727f143384c0f2bfb297a80df5
Sergey Chuguy <mr.chuguy@gmail.com>
2022-09-04 18:35:59 +0300

    finish first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+createGallery();
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`,
+    {
+      onShow: (instance) => {
+        const keyClick = function (event) {
+          if (event.key === "Escape") {
+            instance.close();
+            document.removeEventListener("keyup", keyClick);
+          }
+        };
+        document.addEventListener("keyup", keyClick);
+      },
+    }
+  );
+  instance.show();
+});
+
+function createGallery() {
+  let markup = galleryItems
+    .map(
+      ({ preview, original, description }) => `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`
+    )
+    .join("\n");
+
+  gallery.insertAdjacentHTML("beforeend", markup);
+}
