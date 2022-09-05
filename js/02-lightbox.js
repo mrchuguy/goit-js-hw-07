2d8f9fc69b666548cb6a4ee776f328a0a58d383f
Sergey Chuguy <mr.chuguy@gmail.com>
2022-09-05 19:23:08 +0300

    start second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
+createGallery();
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
+  let gallery = new SimpleLightbox(".gallery a");
+  gallery.on("show.simplelightbox", function () {
+    // do something…
+  });
+});
+
+function createGallery() {
+  let markup = galleryItems
+    .map(
+      ({ preview, original, description }) => `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+    )
+    .join("\n");
+
+  gallery.insertAdjacentHTML("beforeend", markup);
+}
 console.log(galleryItems);
